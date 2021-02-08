f5e0675f9c6c6f415368e545a28443db9c8681df
Dzmitry Rudak <dzm.rudak@gmail.com>
2021-02-08 12:27:10 +0100

    Task 8 has been fixed

--- src/by/rudakd/lesson3/task8/Task8.java
+++ src/by/rudakd/lesson3/task8/Task8.java
@@ -5,14 +5,8 @@ public class Task8 {
     public static void main(String[] args) {
         int a = 10;
         int b = 15;
-        double fact = 1;
-        double searchableFact = 1;
-        for (int num = 1; num <= b; num++){
-            fact *= num;
-            if(num >= a) {
-                searchableFact *= fact;
-            }
-        }
-        System.out.println(searchableFact);
+        int fact = 1;
+        for (; b >= a; fact *= b--){ }
+        System.out.println("Факториал числа в границах от 10 до 15 = " + fact);
     }
 }
