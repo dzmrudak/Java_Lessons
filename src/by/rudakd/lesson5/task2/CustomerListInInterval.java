34c4748dda39b79dcbfbe3b45ef17189a5b72025
dzmrudak <51719312+dzmrudak@users.noreply.github.com>
2021-02-27 00:53:48 +0100

    Update CustomerListInInterval.java

--- src/by/rudakd/lesson5/task2/CustomerListInInterval.java
+++ src/by/rudakd/lesson5/task2/CustomerListInInterval.java
@@ -11,34 +11,18 @@ public class CustomerListInInterval {
         }
     }
 
-    public static Customer[] customerListInInterval(Customer[] array) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the initial value of the Interval: ");
-        int startOfInterval = sc.nextInt();
-        System.out.print("Enter the end value of the Interval: ");
-        int endOfInterval = sc.nextInt();
-        while (endOfInterval < startOfInterval) {
-            System.out.println();
-            System.out.println("The endOfInterval should be >= the startOfInterval");
-            System.out.print("Enter the end value of the Interval: ");
-            endOfInterval = sc.nextInt();
-        }
+    public static Customer[] customerListInInterval(int startOfInterval, int endOfInterval, Customer[] array) {
 
         int countOfNewArrayLength = 0;
         int indexOfNewArray = 0;
         for (int i = 0; i < array.length; i++) {
-            if ((array[i].creditCardId < startOfInterval) || (array[i].creditCardId > endOfInterval)) {
-                continue;
-            } else {
-                countOfNewArrayLength++;
+            if ((array[i].creditCardId >= startOfInterval) && (array[i].creditCardId <= endOfInterval)) {
+                countOfNewArrayLength++;;
             }
         }
         Customer[] arrayInInterval = new Customer[countOfNewArrayLength];
         for (int i = 0; i < array.length; i++) {
-            if ((array[i].creditCardId < startOfInterval) || (array[i].creditCardId > endOfInterval)) {
-                continue;
-            } else {
+            if ((array[i].creditCardId >= startOfInterval) && (array[i].creditCardId <= endOfInterval)) {
                 arrayInInterval[indexOfNewArray] = array[i];
                 indexOfNewArray++;
             }
@@ -46,8 +30,8 @@ public class CustomerListInInterval {
         return arrayInInterval;
     }
 
-    public static void printArrayInInterval(Customer[] array) {
-        Customer[] arrayToPrint = customerListInInterval(array);
+    public static void printArrayInInterval(int startOfInterval, int endOfInterval, Customer[] array) {
+        Customer[] arrayToPrint = customerListInInterval(startOfInterval, endOfInterval, array);
         for (int i = 0; i < arrayToPrint.length; i++) {
             System.out.println(arrayToPrint[i]);
         }
