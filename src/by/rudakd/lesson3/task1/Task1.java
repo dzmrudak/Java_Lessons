584f717f16fc5ec5ad2fe953e3758a3137038df1
dzmrudak <51719312+dzmrudak@users.noreply.github.com>
2021-02-26 21:21:48 +0100

    Update Task1.java

--- src/by/rudakd/lesson3/task1/Task1.java
+++ src/by/rudakd/lesson3/task1/Task1.java
@@ -9,15 +9,19 @@ public class Task1 {
         System.out.print("Enter the array length: ");
         int arrayLength = scan.nextInt();
         int array[] = new int[arrayLength];
-        int maxElement = 0;
+        if (arrayLength == 0) {
+            System.out.println("Массив пуст");
+        } else {
+            int maxElement = array[0];
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] =  (int)(Math.random() * 21); //заполнение массива случайными целыми числами от 0 до 20
-            System.out.print(array[i] + " "); //Вывод массива на экран
+            for (int i = 0; i < array.length; i++) {
+                array[i] = (int) (Math.random() * 21); //заполнение массива случайными целыми числами от 0 до 20
+                System.out.print(array[i] + " "); //Вывод массива на экран
+            }
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] > maxElement) maxElement = array[j]; //Определение значения максимального элемента массиав
+            }
+            System.out.println("\nThe maximum value of the array is: " + maxElement);
         }
-        for (int j = 0; j < array.length; j++) {
-           if(array[j] > maxElement) maxElement = array[j]; //Определение значения максимального элемента массиав
-        }
-        System.out.println("\nThe maximum value of the array is: " + maxElement);
     }
 }
