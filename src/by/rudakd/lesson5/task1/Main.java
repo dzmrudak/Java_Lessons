8dd9beb35379b5e5fadd27cf4f1a0d991b343ea6
dzmrudak <51719312+dzmrudak@users.noreply.github.com>
2021-02-27 00:34:01 +0100

    Update Main.java

--- src/by/rudakd/lesson5/task1/Main.java
+++ src/by/rudakd/lesson5/task1/Main.java
@@ -3,14 +3,15 @@ package by.rudakd.lesson5.task1;
 
 public class Main {
     public static void main(String[] args) {
-        TimePeriod timePeriod1 = new TimePeriod(362);
-        TimePeriod timePeriod2 = new TimePeriod(1, 2, 3);
-        TimePeriod timePeriod3 = new TimePeriod(); // Ручной ввод секунд, минут, часов
+        TimePeriod timePeriod1 = new TimePeriod(1, 2, 3);
+        TimePeriod timePeriod2 = new TimePeriod(0, 0, 6);
+        TimePeriod timePeriod3 = new TimePeriod(0, 0, 5);
 
         TimePeriodService.printCalculateSeconds(timePeriod1);
         TimePeriodService.printCalculateSeconds(timePeriod2);
         TimePeriodService.printCalculateSeconds(timePeriod3);
-        TimePeriodService.compareTwoTimePeriods(timePeriod2, timePeriod3);
+        System.out.println();
+        timePeriod2.compareTwoTimePeriods(timePeriod3);
         TimePeriodService.printPeriodData(timePeriod2);
     }
 }
