e9c2dad0c0eda55756b71d2b6d394369f2a7bc90
dzmrudak <51719312+dzmrudak@users.noreply.github.com>
2021-02-27 14:30:44 +0100

    Update Demo.java

--- src/by/rudakd/lesson6/Demo.java
+++ src/by/rudakd/lesson6/Demo.java
@@ -5,37 +5,31 @@ import java.util.Scanner;
 public class Demo {
     public static void main(String[] args) {
 
-        ArrayService el1 = new ArrayService();
+        ArrayService el1 = new ArrayService(5);
 
-        //Получить текущую длину массива
-        el1.fillArray(el1.arrayLength);
-        System.out.println(el1.toString());
+        System.out.println("Получить текущую длину массива");
         System.out.println("Размер массива = " + el1.getLength());
+        System.out.println();
 
-        //Добавить элемент. Длина массива должна увеличится на 1.
+        System.out.println("Добавить элемент. Длина массива должна увеличится на 1.");
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите элемент, который будет занесен в массив: ");
         Integer element = scan.nextInt();
         System.out.println("Длина нового массива =" + el1.addElement(element));
-        System.out.println(el1.toString());
+        System.out.println();
 
-        //Получить элемент по индексу
-        System.out.print("Введите значение искомого индекса: ");
-        int index = scan.nextInt();
-        System.out.println("Элемент под индексом " + index + " = " + el1.getElement(index));
+        System.out.println("Получить элемент по индексу");
+        System.out.println(el1.getElement(5));
+        System.out.println();
 
-
-        //Удалить все текущие элементы (длина массива будет 0 после этой операции)
+        System.out.println("Удалить все текущие элементы (длина массива будет 0 после этой операции");
         el1.removeAllElements();
         System.out.println("Массив без элементов: " + el1.toString());
+        System.out.println();
 
-        //Вернуть массив где находятся все текущие элементы
+        System.out.println("Вернуть массив где находятся все текущие элементы");
         el1.getAllElements();
-        System.out.println("Массив с восстановленными элементами: " + el1.toString());
-
-
-
-
-
+        System.out.println("Массив: " + el1.toString());
+    }
     }
 }
